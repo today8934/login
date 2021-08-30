a612a17c28732290d31d072eb1fa3c8ac1a4edcd
today8934 <today8934@gmail.com>
2021-08-30 15:30:37 +0900

    Session

---
+++ src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,46 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        //세션 생성
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        member.setId(1L);
+        member.setLoginId("today8934");
+        member.setPassword("qwer1234!");
+        member.setName("Wooksang");
+        sessionManager.createSession(member, response);
+
+        //요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        //세션 조회
+        Object result = sessionManager.getSession(request);
+        System.out.println("member = " + member);
+        System.out.println("result = " + result);
+        assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        assertThat(expired).isNull();
+    }
+
+}
